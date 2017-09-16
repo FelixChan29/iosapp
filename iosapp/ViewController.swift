f6c90212df42847beecf2da12f665122db7b1769
FelixChan29 <chanyiusum29@hotmail.com>
2017-09-16 23:51:21 +0800

    hello world 2

---
+++ iosapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iosapp
+//
+//  Created by Felix Chan on 16/9/2017.
+//  Copyright © 2017 Felix Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print ("Hello World 2")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
